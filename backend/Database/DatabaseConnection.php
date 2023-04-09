aeb58573ff3a3e09e787a5717f3dc6de16e77986
abdallah <abdallahelmekawy6@gmail.com>
2023-04-09 03:51:51 +0200

    database connection calss

---
+++ backend/Database/DatabaseConnection.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * class DatabaseConnection
+ * 
+ * Represents a connection to a MySQL database.
+ */
+class DatabaseConnection
+{
+    private $pdo;
+
+    /**
+     * DatabaseConnection constructor.
+     *
+     * @param [string] $host the host name of the database server
+     * @param [string] $dbname the name of the database
+     * @param [string] $username the username to connect to the database
+     * @param [string] $password the password to connect to the database
+     */
+    public function __construct($host, $dbname, $username, $password)
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        } catch (\PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+    /**
+     * Returns the PDO object
+     *
+     * @return PDO The PDO instance.
+     */
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+}
