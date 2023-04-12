30232438ea453e82728f4abfb47e1ac8c1712f5c
abdallah <abdallahelmekawy6@gmail.com>
2023-04-12 13:25:34 +0200

    change remote api host

--- frontend/src/constants/api.tsx
+++ frontend/src/constants/api.tsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://abdallahscandi.000webhostapp.com/api";
+export const API_BASE_URL = "https://abdallah.lovestoblog.com/api";
 export const GET_PRODUCTS = `/products`;
 export const ADD_PRODUCT = `/product/create`;
 export const MASS_DELETE_PRODUCTS = `/products/delete`;
