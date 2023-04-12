691957618ce605e7e0b798ede7da1a9b21d14b4a
abdallah <abdallahelmekawy6@gmail.com>
2023-04-12 21:08:23 +0200

    new https api host

--- frontend/src/constants/api.tsx
+++ frontend/src/constants/api.tsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://164.92.134.168/api";
+export const API_BASE_URL = "https://abdallah.bio/api";
 export const GET_PRODUCTS = `/products`;
 export const ADD_PRODUCT = `/product/create`;
 export const MASS_DELETE_PRODUCTS = `/products/delete`;
